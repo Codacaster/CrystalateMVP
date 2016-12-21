f4c29fc90827b0d53b4498d5fe8a406d243e2e8a
Mark White <mark@codacast.com>
2016-12-21 13:49:25 +0000

    First Upload
    First Upload to Github

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalateMVP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
